f4c3a85cbc91aadedd4ada68fc587ddc7305dac7
Sadman Adib <sadmanadib67@gmail.com>
2022-08-24 22:09:43 +0600

    Initial Commit

---
+++ Game of Life/Game_of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_of_LifeApp.swift
+//  Game of Life
+//
+//  Created by Sadman Adib on 24/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Game_of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
