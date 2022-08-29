93722632f09758b9973f1bfc6a3a686e807557d4
Sadman Adib <sadmanadib67@gmail.com>
2022-08-29 12:25:49 +0600

    Added game rules to GuideView

--- Game of Life/GuideView/GuideView.swift
+++ Game of Life/GuideView/GuideView.swift
@@ -23,6 +23,28 @@ struct GuideView: View {
                     
                     MarkerView(title: "DEAD", cellColor: .black)
                     
+                    Section {
+                        VStack(alignment: .leading){
+                            Text("Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent")
+                                .foregroundColor(.yellow)
+                            Divider()
+                            Text("1. Any live cell with fewer than two live neighbours dies, as if by underpopulation")
+                            Divider()
+                            Text("2. Any live cell with two or three live neighbours lives on to the next generation.")
+                            Divider()
+                            Text("3. Any live cell with more than three live neighbours dies, as if by overpopulation.")
+                            Divider()
+                            Text("4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.")
+                        }
+                        .foregroundColor(.white)
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.5)
+                    } header: {
+                        Text("Rules :")
+                            .font(.title3)
+                    }
+
+                    
                     Spacer()
                     
                     Text("Tap on the grid cells to set it to either Dead or Alive")
@@ -60,7 +82,7 @@ struct MarkerView: View {
     var body: some View {
         HStack(spacing: 20){
             Rectangle()
-                .frame(width: 80, height: 80)
+                .frame(width: 50, height: 50)
                 .foregroundColor(cellColor)
             
             Text(title)
